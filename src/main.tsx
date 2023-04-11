60ec7bc85035fc09231238bcf7d710e0da0178b9
Igor Koneshov <igorkoneshov@mail.ru>
2023-04-11 17:33:15 +0400

    feat: add variable color

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import "./index.scss";
 import router from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Main.scss";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className="main">
+      <h2 className="main__title">Рекомендуемые темы</h2>
+
+      <div className="recomended-theme">theme</div>
+    </main>
+  );
 };
 
 export default Main;
