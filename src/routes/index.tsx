69d7693ce5dcde288dcfa1a6e98fe8f4d8cc3d64
Igor Koneshov <igorkoneshov@mail.ru>
2023-04-17 01:26:37 +0400

    fix: add base route from github pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,41 +7,46 @@ const Main = lazy(() => import("../pages/MainPage/Main"));
 const SearchPage = lazy(() => import("../pages/SearchPage/SearchPage"));
 const Tasks = lazy(() => import("../pages/TasksPage/Tasks"));
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<Spin />}>
+          <Layout />
+        </Suspense>
+      ),
+      children: [
+        {
+          path: "/",
+          element: (
+            <Suspense fallback={<Spin />}>
+              <Main />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/tasks",
+          element: (
+            <Suspense fallback={<Spin />}>
+              <Tasks />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/search",
+          element: (
+            <Suspense fallback={<Spin />}>
+              <SearchPage />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <Suspense fallback={<Spin />}>
-        <Layout />
-      </Suspense>
-    ),
-    children: [
-      {
-        path: "/",
-        element: (
-          <Suspense fallback={<Spin />}>
-            <Main />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/tasks",
-        element: (
-          <Suspense fallback={<Spin />}>
-            <Tasks />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/search",
-        element: (
-          <Suspense fallback={<Spin />}>
-            <SearchPage />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-]);
+    basename: "/developer-portal",
+  }
+);
 
 export default router;
