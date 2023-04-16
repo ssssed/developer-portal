ad877723ae38cb59f5c9cd97d8049e31fbf08b6a
Igor Koneshov <igorkoneshov@mail.ru>
2023-04-16 16:42:52 +0400

    feat: create date format hook

---
+++ src/hook/useDateFormat.ts
@@ -0,0 +1,8 @@
+export const useDateFormat = (data: Nullable<number>) => {
+  if (null) return null;
+  const date = new Date(data);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear().toString();
+  return `${day}.${month}.${year}`;
+};
