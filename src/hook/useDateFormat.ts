de99000b733d0b295312e6c4d1076f491fe8b885
Igor Koneshov <igorkoneshov@mail.ru>
2023-04-16 23:39:20 +0400

    fix: type of date

--- src/hook/useDateFormat.ts
+++ src/hook/useDateFormat.ts
@@ -1,5 +1,5 @@
-export const useDateFormat = (data: Nullable<number>) => {
-  if (null) return null;
+export const useDateFormat = (data: number | null | undefined) => {
+  if (!data) return null;
   const date = new Date(data);
   const day = date.getDate().toString().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
